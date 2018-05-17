25910b984caf53de2bb09d1006c7f8d16680acd4
jbfink <john.fink@gmail.com>
2018-05-17 11:05:41 -0400

    Header mods, commands for RFID, print-pi.sh

--- receipt-generator/print-pi.sh
+++ receipt-generator/print-pi.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #qiv `ls images | shuf -n 1`
 #dadadodo -c 4 Haraway.txt | lp
-lp -o cpi=18 texts/header.txt
+lp -o cpi=18 /home/pi/dhguelph2018/receipt-generator/texts/header.txt
 #lp -o cpi=18 texts/emotional-labour.txt
-lp -o cpi=18 texts/$1".txt"
+lp -o cpi=18 /home/pi/dhguelph2018/receipt-generator/texts/$1".txt"
 #echo "lp -o cpi=18 texts/"$1".txt"
 #lp images/`ls images | shuf -n 1`
 #lp images/sadrobot.jpg
