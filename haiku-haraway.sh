42b2989b9322fe177fd670556fae5efa571e41b0
jbfink <john.fink@gmail.com>
2018-05-17 10:05:12 -0400

    Added text for each ticket.

--- haiku-haraway.sh
+++ haiku-haraway.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker run -v /home/jbfink/guelph/:/tmp/guelph guelph-haiku /tmp/guelph/haiku-generator/haiku.py /tmp/guelph/receipt-generator/embargoed/Haraway.txt
+#docker run -v /home/jbfink/guelph/:/tmp/guelph guelph-haiku /tmp/guelph/haiku- generator/haiku.py /tmp/guelph/receipt-generator/embargoed/ `ls /tmp/guelph/receipt-generator/embargoed/ | shuf -n 1`
