646be457145a7cb04dbeb20aba7c4592399c18df
jbfink <john.fink@gmail.com>
2018-05-17 09:29:34 -0400

    Various. Receipt generator scripts, some images for receipt generator, a shell script to produce haikus from the Haraway text (via a Docker container).

---
+++ haiku-haraway.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run -v /home/jbfink/guelph/:/tmp/guelph guelph-haiku /tmp/guelph/haiku-generator/haiku.py /tmp/guelph/receipt-generator/embargoed/Haraway.txt
--- receipt-generator/print.sh
+++ receipt-generator/print.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #qiv `ls images | shuf -n 1`
 #dadadodo -c 4 Haraway.txt | lp
-lp images/`ls images | shuf -n 1`
+lp -o cpi=18 texts/header.txt
+lp -o cpi=18 texts/emotional-labour.txt
+#lp images/`ls images | shuf -n 1`
+lp images/sadrobot.jpg
 dadadodo -c 2 embargoed/Haraway.txt | lp -o cpi=15
